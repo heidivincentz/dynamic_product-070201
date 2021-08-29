bf7e7f6acc2217aaf9f2007b62b2e49c3e75e375
Heidi Vincentz <heid344m@stud.kea.dk>
2021-08-29 14:43:21 +0200

    ??

---
+++ js/product.js
@@ -0,0 +1,19 @@
+const url = "https://kea-alt-del.dk/t7/api/products/1163";
+// fetch data
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
+// populate the page
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".breadcrumbs .brand").textContent = product.brandname;
+  document.querySelector(".breadcrumbs .productname").textContent =
+    product.productdisplayname;
+
+  // replace image
+  document.querySelector(
+    "img.productimage"
+  ).src = `https://kea-alt-del.dk/t7/images/jpg/1000/${product.id}.jpg`;
+  document.querySelector("img.productimage").alt = product.productdisplayname;
+}
